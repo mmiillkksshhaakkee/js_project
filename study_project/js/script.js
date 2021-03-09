5175b5880e85e68598f62940a16632f5fa49e2dd
Anna <twistoliver735@gmail.com>
2021-03-09 17:42:28 +0300

    first commit

---
+++ study_project/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(typeof(result));
+
+// const answer = prompt("Вам есть 18?", "сколько");
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Your first name?', '');
+// answers[1] = prompt('Your last name?', '');
+// answers[2] = prompt('Your age?', '');
+
