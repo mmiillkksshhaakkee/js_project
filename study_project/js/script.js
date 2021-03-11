e8f18ff44d4dfa8f6eccc5852982ae0b1ac47678
Anna <twistoliver735@gmail.com>
2021-03-11 04:23:04 +0300

    script.js destruct

--- study_project/js/script.js
+++ study_project/js/script.js
@@ -12,3 +12,42 @@
 // answers[1] = prompt('Your last name?', '');
 // answers[2] = prompt('Your age?', '');
 
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+
+    makeTest: function(){
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+// ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options));
+// console.log(Object.keys(options).length);
+
+// let counter = 0;
+
+// for (let key in options){
+//     // if(typeof(options[key]) === 'object'){
+//     //     for(let i in options[key]){
+//     //         console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//     //         counter++;
+//     //     } 
+//     // } else {
+//     //     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     //}
+    
+// }
+
+// console.log(counter);
+
