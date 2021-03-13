37c8c4e72e95579622915b5545383218fcbff012
Anna <twistoliver735@gmail.com>
2021-03-13 23:33:21 +0300

    object.create try

--- study_project/js/script.js
+++ study_project/js/script.js
@@ -51,7 +51,7 @@
 
 // console.log(counter);
 
-let someObj = {
+/* let someObj = {
     a: 2,
 };
  
@@ -69,5 +69,25 @@ let someObj = {
   console.log(objCopy); // { c: 3 }
 
   let clone = Object.defineProperties({}, Object.getOwnPropertyDescriptors(someObj));
-  
+
   console.log(JSON.stringify(Object.getOwnPropertyDescriptors(obj), null, 2 ));
+  */
+
+/* var v1 = '123';
+var v2 = true;
+var v3 = 10;
+var v4 = Symbol('foo')
+
+var obj = Object.assign({}, v1, null, v2, undefined, v3, v4);
+// Примитивы будут обёрнуты, а null и undefined - проигнорированы.
+// Обратите внимание, что собственные перечисляемые свойства имеет только обёртка над строкой.
+console.log(obj); // { "0": "1", "1": "2", "2": "3" } */
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+const john = Object.create(soldier);
+console.log(john.armor); // где дескрипторы свойств???
+console.log(JSON.stringify(Object.getOwnPropertyDescriptor(john, 'health'), null, 2 ));
