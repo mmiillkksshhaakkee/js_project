36a306da7ecdeb6754270acdd52aa0b7e9db9755
Anna <twistoliver735@gmail.com>
2021-03-27 22:26:58 +0300

    delegation and classList learning

--- study_project/study_2nd_module/script.js
+++ study_project/study_2nd_module/script.js
@@ -1,14 +1,44 @@
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
 // console.log(btns[0].classList.length);
 // console.log(btns[0].classList.item(0));
 
-console.log(btns[1].classList.add('red'));
+// console.log(btns[1].classList.add('red'));
 //console.log(btns[0].classList.remove('blue'));
 //console.log(btns[0].classList.toggle('blue'));
 
-if(btns[1].classList.contains('red')){
-    console.log('red');
-} else {
-    console.log('no');
-}
+// if(btns[1].classList.contains('red')){
+//     console.log('red');
+// } else {
+//     console.log('no');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')){
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+// console.log(btns[0].classList);
+
+// wrapper.addEventListener('click', (e) => {
+//     if(e.target && e.target.tagName == "BUTTON"){
+//         console.log("Hello");
+//     }
+// });
+
+wrapper.addEventListener('click', (e) => {
+    if(e.target && e.target.matches('button.red')){
+        console.log("Hello");
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
